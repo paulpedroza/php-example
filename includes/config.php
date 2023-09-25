a4af884c9129bc96246f915d0f71d2997edea5d2
Paul Pedroza <andres.pedroza85@gmail.com>
2023-09-25 19:31:35 +0000

    .

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website (Test)',
+        'name' => 'Simple PHP Website (Test1)',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
